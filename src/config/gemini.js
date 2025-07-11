0501d8756fe846fa94816f64f7d9008086dc9633
Vikas Kumar Singh <vikaskumarsingh14aug@gmail.com>
2025-07-11 19:13:30 +0530

    initial commit

---
+++ src/config/gemini.js
@@ -0,0 +1,39 @@
+// To run this code you need to install the following dependencies:
+// npm install @google/genai mime
+// npm install -D @types/node
+
+import {
+  GoogleGenAI,
+} from '@google/genai';
+
+async function main(prompt) {
+  const ai = new GoogleGenAI({
+    apiKey: 'AIzaSyAJU_SIirQGbj97J-nrDNCtq2XCLyHaq68',
+  });
+  const config = {
+    thinkingConfig: {
+      thinkingBudget: -1,
+    },
+    responseMimeType: 'text/plain',
+  };
+  const model = 'gemini-2.5-pro';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
+        {
+          text: prompt,
+        },
+      ],
+    },
+  ];
+ const result = await ai.models.generateContent({
+    model,
+    config,
+    contents,
+  });
+
+  return result.text;
+}
+
+export default main;
